f3b9a93ae9bbca621fc1971ee88fb45b30cfabf0
Brent Beardsley <bbeardsley@figure.com>
2020-04-14 20:16:32 -0600

    added GetFilteredValues and ReverseValues

--- histkeep.go
+++ histkeep.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"os"
 	"regexp"
+	"sort"
 )
 
 // HistKeep main struct
@@ -13,7 +14,9 @@ type HistKeep interface {
 	AddValue(value string) error
 	RemoveValue(value string) error
 	GetValues() ([]string, error)
+	GetFilteredValues(filterFunc func(string) bool) ([]string, error)
 	ClearValues() error
+	ReverseValues([]string) []string
 }
 
 type histKeep struct {
@@ -92,6 +95,24 @@ func (histkeep *histKeep) GetValues() ([]string, error) {
 	return lines, nil
 }
 
+func (histkeep *histKeep) GetFilteredValues(filterFunc func(string) bool) ([]string, error) {
+	values, err := histkeep.GetValues()
+	if err != nil {
+		return nil, err
+	}
+	filtered := []string{}
+	for _, line := range values {
+		if filterFunc(line) {
+			filtered = append(filtered, line)
+		}
+	}
+	return filtered, nil
+}
+
+func (histkeep *histKeep) ReverseValues(values []string) []string {
+	return reverseValues(values)
+}
+
 func readLines(path string, ignoreValue string, format regexp.Regexp) ([]string, error) {
 	_, err := os.Stat(path)
 	if os.IsNotExist(err) {
@@ -139,4 +160,28 @@ func writeLines(path string, lines []string) error {
 		}
 	}
 	return w.Flush()
+}
+
+type stringValue struct {
+	value string
+	index int
+}
+
+type byIndex []stringValue
+
+func (b byIndex) Len() int           { return len(b) }
+func (b byIndex) Less(i, j int) bool { return b[i].index < b[j].index }
+func (b byIndex) Swap(i, j int)      { b[i], b[j] = b[j], b[i] }
+
+func reverseValues(values []string) []string {
+	stringValues := make([]stringValue, 0)
+	for i := 0; i < len(values); i++ {
+		stringValues = append(stringValues, stringValue{values[i], i})
+	}
+	sort.Sort(sort.Reverse(byIndex(stringValues)))
+	items := make([]string, 0)
+	for i := 0; i < len(stringValues); i++ {
+		items = append(items, stringValues[i].value)
+	}
+	return items
 }
